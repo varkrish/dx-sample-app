62712d06330543e7bae924b451db58a344ec8cc6
varkrish <--get-all>
2022-02-14 10:41:03 +1100

    Base line version

--- buildandpush.sh
+++ buildandpush.sh
@@ -1,7 +1,7 @@
-#!/bin/sh
+!/bin/sh
 mvn clean install
 HOST=$(oc get route default-route -n openshift-image-registry --template='{{ .spec.host }}')
 docker login -u $(oc whoami) -p $(oc whoami -t) $HOST
-docker build -f src/main/docker/Dockerfile.jvm -t $HOST/address-dev/dx-sample-app:1.0.0 .
-docker push  $HOST/address-dev/dx-sample-app:1.0.0
+docker build -f src/main/docker/Dockerfile.jvm -t $HOST/sample/dx-sample-app:1.0.0 .
+docker push  $HOST/sample/dx-sample-app:1.0.0
 helm upgrade --install dx-sample-app --set is.enabled=false --set istag.enabled=false chart/
